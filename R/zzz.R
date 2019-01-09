0d1adec3f0e51d06ddde40aa23f416308cd6228d
burchill <zach.burchill+gh@gmail.com>
2019-01-08 20:58:04 -0500

    added other default catchr options

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,10 @@
 .onLoad <- function(libname, pkgname) {
   # Unless already set, the default plan is to collect and muffle
-  catchr.default_plan <- getOption("catchr.default_plan",
-                                   list("collect","muffle"))
+  catchr.default_plan <- getOption("catchr.default_plan", catchr.default_plan)
   # Set the default just in case it isn't already
   options("catchr.default_plan" = catchr.default_plan)
+
+  catchr.warn_about_terms <- getOption("catchr.warn_about_terms", catchr.warn_about_terms)
+  catchr.bare_if_possible <- getOption("catchr.warn_about_terms", catchr.bare_if_possible)
+  catchr.drop_empty <- getOption("catchr.warn_about_terms", catchr.drop_empty)
 }
