ac7f4d5335c380908f1e04a492c1844adcccefc2
burchill <zach.burchill+gh@gmail.com>
2019-01-10 18:03:35 -0500

    updated docs and fn names

--- tests/testthat/test-testing.R
+++ tests/testthat/test-testing.R
@@ -12,13 +12,13 @@ test_that("Namespaces and environments", {
 
   # If you define it in the function, it should give a warning
   expect_warning(
-    res1 <- clean_cond_input(d1 = function(x) { return(sup) },
+    res1 <- check_and_clean_input(d1 = function(x) { return(sup) },
                              spec_names = taboo)
   )
   expect_equal(res1$kwargs$d1(""), sup)
 
   expect_silent(
-    res2 <- clean_cond_input(d2 = samenamespace,
+    res2 <- check_and_clean_input(d2 = samenamespace,
                              d3 = diffnamespace,
                              spec_names = taboo)
   )
@@ -34,14 +34,14 @@ test_that("Explictly package-named functions", {
   acosh <- function(x) { "dummy" }
 
   expect_warning(
-    res1 <- clean_cond_input(d1 = acosh, spec_names = "acosh")
+    res1 <- check_and_clean_input(d1 = acosh, spec_names = "acosh")
   )
   # the kwarg has arg_pos attributes
   expect_failure(expect_equal(res1$kwargs$d1, "acosh"))
   expect_equivalent(res1$kwargs$d1, "acosh")
 
   expect_silent(
-    res2 <- clean_cond_input(d1 = base::acosh, spec_names = "acosh")
+    res2 <- check_and_clean_input(d1 = base::acosh, spec_names = "acosh")
   )
   expect_equal(res2$kwargs$d1(10), base::acosh(10))
 
@@ -52,7 +52,7 @@ test_that("Function names are not masked", {
   sup <- function(x) { function(y) {return("dummy")} }
 
   expect_silent(
-    res <- clean_cond_input(d1 = sup(""), spec_names = "sup")
+    res <- check_and_clean_input(d1 = sup(""), spec_names = "sup")
   )
   expect_equal(res$kwargs$d1(""), "dummy")
 
