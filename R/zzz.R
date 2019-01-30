cfc86245d789c5ac9b857be9e98f594f0bfc9e08
burchill <zach.burchill+gh@gmail.com>
2019-01-30 18:08:52 -0500

    cleaned up default values, moved them to catchr_original_default_values

--- R/zzz.R
+++ R/zzz.R
@@ -1,18 +1,9 @@
 .onLoad <- function(libname, pkgname) {
-  # Unless already set, the default plan is to collect and muffle
-  catchr.default_plan <- getOption("catchr.default_plan", list("collect", "muffle"))
-  # By default, you'll be warned about catchrs DSL
-  catchr.warn_about_terms <- getOption("catchr.warn_about_terms", TRUE)
-  # By default, you want to always return a list when you can collect
-  catchr.bare_if_possible <- getOption("catchr.bare_if_possible", FALSE)
-  # By default you don't want to drop stuff
-  catchr.drop_empty <- getOption("catchr.drop_empty", FALSE)
-
-
+  ov <- catchr_original_default_values
   # Set the defaults just in case they aren't already
-  options("catchr.default_plan" = catchr.default_plan)
-  options("catchr.warn_about_terms" = catchr.warn_about_terms)
-  options("catchr.bare_if_possible" = catchr.bare_if_possible)
-  options("catchr.drop_empty" = catchr.drop_empty)
-
+  options("catchr.default_plan" = getOption("catchr.default_plan", ov$catchr.default_plan))
+  options("catchr.warn_about_terms" = getOption("catchr.warn_about_terms", ov$catchr.warn_about_terms))
+  options("catchr.bare_if_possible" = getOption("catchr.bare_if_possible", ov$catchr.bare_if_possible))
+  options("catchr.drop_empty_conds" = getOption("catchr.drop_empty_conds", ov$catchr.drop_empty_conds))
+  ov <- NULL
 }
